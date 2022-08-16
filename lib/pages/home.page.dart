37cdbb5b3b24a49a47ddfab40a3006830124ac24
Lucas Romano <lucasromano@pop-os.localdomain>
2022-08-16 14:29:11 -0300

    feat: load full list

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -24,6 +24,14 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    void getPosts() {
+      // post = postRepository.fetchPost(2);
+      posts = postRepository.fetchAllPosts();
+      setState(() {
+        requesting = true;
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Agendamentos'),
@@ -51,34 +59,50 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(8),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             ElevatedButton(
               child: const Text('Busca na API'),
-              onPressed: () {
-                // post = postRepository.fetchPost(2);
-                posts = postRepository.fetchAllPosts();
-                setState(() {
-                  requesting = true;
-                });
-              },
+              onPressed: () => getPosts(),
             ),
             if (requesting)
               FutureBuilder<List<Post>>(
                 future: posts,
                 builder: (_, snapshot) {
                   if (snapshot.hasData) {
-                    return Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        children: [
-                          Text('ID -> ${snapshot.data![0].id}'),
-                          Text('Title -> ${snapshot.data![0].title}'),
-                          Text('Body -> ${snapshot.data![0].body}'),
-                          const Divider(color: Color(0xF7F7F7FF)),
-                          Text('ID -> ${snapshot.data![1].id}'),
-                          Text('Title -> ${snapshot.data![1].title}'),
-                          Text('Body -> ${snapshot.data![1].body}'),
-                        ],
+                    return Expanded(
+                      child: Container(
+                        height: MediaQuery.of(context).size.height,
+                        padding: const EdgeInsets.all(16),
+                        child: ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (_, int index) {
+                            return Column(
+                              children: [
+                                Text('ID --> ${snapshot.data![index].id}'),
+                                Text(
+                                    'Title --> ${snapshot.data![index].title}'),
+                                Text('Body --> ${snapshot.data![index].body}'),
+                                Text(
+                                    'UserId --> ${snapshot.data![index].userId}'),
+                                const Divider(color: Color(0xF7F7F7FF)),
+                              ],
+                            );
+                          },
+                        ),
+                        // child: Column(
+                        //   children: [
+                        //     Text('ID -> ${snapshot.data![0].id}'),
+                        //     Text('Title -> ${snapshot.data![0].title}'),
+                        //     Text('Body -> ${snapshot.data![0].body}'),
+                        //     const Divider(color: Color(0xF7F7F7FF)),
+                        //     Text('ID -> ${snapshot.data![1].id}'),
+                        //     Text('Title -> ${snapshot.data![1].title}'),
+                        //     Text('Body -> ${snapshot.data![1].body}'),
+                        //   ],
+                        // ),
                       ),
                     );
                   } else if (snapshot.hasError) {
